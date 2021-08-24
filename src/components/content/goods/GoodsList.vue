3aceeb4b4f97f312909547a284baba707d507a24
whd <1371686415@qq.com>
2021-08-25 01:44:10 +0800

    '第一次提交'

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="goods">
+    <goods-list-item v-for="item in goods" :goods-item="item" class="item"/>
+  </div>
+</template>
+
+<script>
+  import GoodsListItem from "@/components/content/goods/GoodsListItem";
+
+export default {
+  name: "GoodsList",
+  components: {
+    GoodsListItem
+  },
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .goods {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 5px;
+    justify-content: space-around;
+  }
+
+  .goods .item {
+    width: 48%;
+  }
+</style>
